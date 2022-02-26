a85a4adc0df0f9d8ef4d80d5d2863c311febd2b1
gayomiiii <97868213+gayomiiii@users.noreply.github.com>
2022-02-26 20:24:45 +0900

    Add files via upload

---
+++ 10171/src/Main.java
@@ -0,0 +1,18 @@
+
+// https://www.acmicpc.net/problem/10171
+// 아래 예제와 같이 고양이를 출력하시오.
+/* 
+  \    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
