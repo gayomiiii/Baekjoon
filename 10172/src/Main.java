3b6e83c1f836c5cf394d6b0afdf5aed61f522d66
gayomiiii <97868213+gayomiiii@users.noreply.github.com>
2022-02-26 20:31:26 +0900

    Add files via upload

---
+++ 10172/src/Main.java
@@ -0,0 +1,20 @@
+
+// https://www.acmicpc.net/problem/10172
+// 아래 예제와 같이 개를 출력하시오.
+/* 
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
