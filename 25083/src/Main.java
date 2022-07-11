b8a98ba7186d90673d60333c8eb9096560a93420
gayomiiii <97868213+gayomiiii@users.noreply.github.com>
2022-07-11 22:19:28 +0900

    Add files via upload

---
+++ 25083/src/Main.java
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/25083
+/*
+ 아래 예제와 같이 새싹을 출력하시오.
+          ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+// 입력은 없다.
+// 새싹을 출력한다.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+        
+		System.out.print(s);
+    }
+}
